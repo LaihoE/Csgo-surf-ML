6b9b70bf381d1f7dcafb3050f43969a8bafff796
PythonBoost <emillaiho@hotmail.fi>
2021-06-28 21:34:02 +0300

    100 lags

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import time
+
+import pandas as pd
+
 from tset import key_check
 from testing import Keys
 import pickle
 keys = Keys()
+
+# con_logfile console.log
+# sv_accelerate 10
+# sv_airaccelerate 800
+
 
 # Load in the 3 models
 with open("C:/Users/emill/PycharmProjects/OWcheaters/script/models/cb_modelv.model", "rb") as f:
@@ -13,9 +21,10 @@ with open("C:/Users/emill/PycharmProjects/OWcheaters/script/models/cb_modelc.mod
     cb_modelc = pickle.load(f)
 
 
-def mainloop():
-    # THE FOLLOWING COMMAND HAS TO BE PUT INTO CONSOLE. It starts logging your console
 
+def mainloop(lastX,lastY,lastZ):
+    # THE FOLLOWING COMMAND HAS TO BE PUT INTO CONSOLE. It starts logging your console
+    global bigdf
     # con_logfile console.log
 
     # P is binded ingame to showpos1 (it gives the positional data)
@@ -23,8 +32,9 @@ def mainloop():
     time.sleep(0.01)
     keys.directKey("p", keys.key_release)
     start = time.time()
+
+    # Read console.log and get the relevant data
     try:
-        # Read console.log and get the relevant data
         with open(r"C:/Program Files (x86)/Steam/steamapps/common/Counter-Strike Global Offensive/csgo/console.log",
                   "r") as f:
             lines = f.read().splitlines()
@@ -36,6 +46,7 @@ def mainloop():
             angles = splitted[1]
             angles = angles.split(" ")
 
+
             class coordinates:
                 X = coords[1]
                 Z = coords[2]
@@ -43,55 +54,88 @@ def mainloop():
                 vertical = angles[1]
                 horizontal = angles[2]
                 keyspressed = key_check()
+                SpeedX=float(X) - float(lastX)
+                SpeedY=float(Y) - float(lastY)
+                SpeedZ=float(Z) - float(lastZ)
+
             # SAFETY key will stop the bot
             if "O" in coordinates.keyspressed:
-                return "O"
+                return "O","0","0"
 
-            predicted_horiz_angle = cb_modelh.predict(
-                [float(coordinates.X), float(coordinates.Y), float(coordinates.Z)])
-            difh=float(coordinates.horizontal)-predicted_horiz_angle
+            def generate_time_lags(df, n_lags):
+                df_n = df.copy()
+                cols = ["X", "Y", "Z"]
+                for c in cols:
+                    for n in range(1, n_lags + 1):
+                        df_n[f"{c}_lag{n}"] = df_n[f"{c}"].shift(n)
+                df_n = df_n.iloc[n_lags:]
+                return df_n
 
-            predicted_vertical_angle = cb_modelv.predict(
-                                [float(coordinates.X), float(coordinates.Y), float(coordinates.Z)])
-            difv = float(coordinates.vertical)-predicted_vertical_angle
 
-            # User specific. Depends on your sens/dpi. Figured it out after some playing with it (0.8 in-game 800dpi)
-            moveh = int(round(difh / 0.0176,0))
-            movev = int(round(difv / 0.0176,0))
 
-            # Can make it move towards the correct angle but not exactly. if constant is 1 then make cursor "teleport"
-            # to the exact position it wants. Similar learning rate in deep learning. Seems to work well with 1
-            lrmoveh=int(round(moveh*1))
-            lrmovev=int(round(movev*1))
+            #print(df)
+            #df = pd.DataFrame()
+            row = pd.DataFrame({"X":float(coordinates.X), "Y":float(coordinates.Y), "Z":float(coordinates.Z),"SpeedX":coordinates.SpeedX,"SpeedY":coordinates.SpeedY,"SpeedZ":coordinates.SpeedZ},index=[0])
+            bigdf = bigdf.append(row,ignore_index=True)
 
-            # Move the mouse
-            keys.directMouse(lrmoveh, -lrmovev)
-            time.sleep(0.0004)
+            input_dim = 100
+            if len(bigdf) > 100:
+                predrow = generate_time_lags(bigdf, input_dim)
 
-            # Predict what key to press (output is 0-2 corresponding to A,W,D)
-            keyp = cb_modelc.predict([float(coordinates.X), float(coordinates.Y), float(coordinates.Z),float(coordinates.horizontal),float(coordinates.vertical)])
+                print(predrow.iloc[-1])
+                lastpred = predrow.iloc[-1]
+                predicted_horiz_angle = cb_modelh.predict(lastpred)
+                #[float(coordinates.X), float(coordinates.Y), float(coordinates.Z),coordinates.SpeedX,coordinates.SpeedY,coordinates.SpeedZ])
+                difh=float(coordinates.horizontal)-predicted_horiz_angle
+                print("DIFH",difh)
+                predicted_vertical_angle = cb_modelv.predict(lastpred)
+                      #[float(coordinates.X), float(coordinates.Y), float(coordinates.Z),coordinates.SpeedX,coordinates.SpeedY,coordinates.SpeedZ])
+                difv = float(coordinates.vertical)-predicted_vertical_angle
 
-            if keyp==0:
-                keyp="A"
-            elif keyp==1:
-                keyp="W"
-            else: keyp="D"
-            # Release the old loop holds
-            keys.directKey("A", keys.key_release)
-            keys.directKey("W", keys.key_release)
-            keys.directKey("D", keys.key_release)
-            # Press down key
-            keys.directKey(keyp)
-            time.sleep(0.001)
-            print(f"Position (X:{float(coordinates.X):.2f} Y:{float(coordinates.Y):.2f} Z: {float(coordinates.Z):.2f})      Moved {difh:.2f} horizontally and {difv:.2f} vertically. Button pressed: {keyp}")
-    except:
-        pass
+                # User specific. Depends on your sens/dpi. Figured it out after some playing with it (0.8 in-game 800dpi)
+                moveh = int(round(difh / 0.0176,0))
+                movev = int(round(difv / 0.0176,0))
 
+                # Can make it move towards the correct angle but not exactly. if constant is 1 then make cursor "teleport"
+                # to the exact position it wants. Similar learning rate in deep learning. Seems to work well with 1
+                lrmoveh=int(round(moveh*0.8))
+                lrmovev=int(round(movev*0.8))
+
+                # Move the mouse
+                keys.directMouse(lrmoveh, -lrmovev)
+                time.sleep(0.0004)
+
+                # Predict what key to press (output is 0-2 corresponding to A,W,D)
+                keyp = cb_modelc.predict(lastpred)
+                    #[float(coordinates.X), float(coordinates.Y), float(coordinates.Z),float(coordinates.horizontal),float(coordinates.vertical),coordinates.SpeedX,coordinates.SpeedY,coordinates.SpeedZ])
+
+                if keyp==0:
+                    keyp="A"
+                elif keyp==1:
+                    keyp="W"
+                else: keyp="D"
+                # Release the old loop holds
+                keys.directKey("A", keys.key_release)
+                keys.directKey("W", keys.key_release)
+                keys.directKey("D", keys.key_release)
+                # Press down key
+                keys.directKey(keyp)
+                time.sleep(0.001)
+                print(f"Position (X:{float(coordinates.X):.2f} Y:{float(coordinates.Y):.2f} Z: {float(coordinates.Z):.2f})      Moved {difh:.2f} horizontally and {difv:.2f} vertically. Button pressed: {keyp}")
+                return coordinates.X,coordinates.Y,coordinates.Z
+    except Exception as e:
+        print(e)
 # Clear the log file to avoid it becoming too large (maybe slower to read if file is bigger?)
 with open(r"C:/Program Files (x86)/Steam/steamapps/common/Counter-Strike Global Offensive/csgo/console.log","w") as f:
     f.close()
 
+time.sleep(2)
+lastX,lastY,lastZ=0,0,0
+bigdf = pd.DataFrame({"X":0, "Y":0, "Z":0,"SpeedX":0,"SpeedY":0,"SpeedZ":0},index=[0])
 for game in range(1000):
-    x = mainloop()
-    if x=="O":
+    try:
+        lastX,lastY,lastZ = mainloop(lastX,lastY,lastZ)
+    except:
+        pass
+    if lastX=="O":
         exit()
--- models_saver.py
+++ models_saver.py
@@ -1,6 +1,6 @@
 import pandas as pd
 bigboi = pd.read_csv("C:/Users/emill/PycharmProjects/OWcheaters/longrun/cords1.csv")
-for i in range(60, 106):
+for i in range(200,249):
     df2=pd.read_csv(f"C:/Users/emill/PycharmProjects/OWcheaters/longrun/cords{i}.csv")
     bigboi = pd.concat([bigboi, df2], ignore_index=True)
 
@@ -9,8 +9,57 @@ from sklearn.model_selection import train_test_split
 from catboost import CatBoostClassifier
 import pickle
 
-# GET OUT THE "P" THAT IS THE KEY FOR GETTING YOUR POSITION (NOT INTERESTED)
 df=bigboi
+"""listaX=[]
+listaY=[]
+listaZ=[]
+
+listaX.append(0)
+listaY.append(0)
+listaZ.append(0)
+for i in range(1,len(df["X"])):
+
+    last=df["X"].iloc[i-1]
+    now=df["X"].iloc[i]
+    speed=now-last
+    listaX.append(speed)
+
+for i in range(1,len(df["Y"])):
+    last=df["Y"].iloc[i-1]
+    now=df["Y"].iloc[i]
+    speed=now-last
+    listaY.append(speed)
+
+for i in range(1,len(df["Z"])):
+    last=df["Z"].iloc[i-1]
+    now=df["Z"].iloc[i]
+    speed=now-last
+    listaZ.append(speed)
+
+df["SpeedX"]=listaX
+df["SpeedY"]=listaY
+df["SpeedZ"]=listaZ
+"""
+
+
+def generate_time_lags(df, n_lags):
+    df_n = df.copy()
+    cols = ["X","Y","Z"]
+    for c in cols:
+        for n in range(1, n_lags + 1):
+            df_n[f"{c}_lag{n}"] = df_n[f"{c}"].shift(n)
+    df_n = df_n.iloc[n_lags:]
+    return df_n
+
+
+input_dim = 100
+
+df = generate_time_lags(df, input_dim)
+print(df)
+
+
+# GET OUT THE "P" THAT IS THE KEY FOR GETTING YOUR POSITION (NOT INTERESTED)
+
 bigkeys=[]
 for x in df["keys"]:
     keys = []
@@ -55,10 +104,10 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.01, random
 print(df)
 
 
-cb_modelh = CatBoostRegressor(iterations=2000,
-                              depth=6,
+cb_modelh = CatBoostRegressor(iterations=500,
+                              depth=8,
                               task_type="CPU",
-                              eval_metric='R2',
+                              eval_metric='MAE',
                               random_seed=42,
                               bagging_temperature=0.2,
                               od_type='Iter',
@@ -66,7 +115,7 @@ cb_modelh = CatBoostRegressor(iterations=2000,
                               od_wait=10,
                               learning_rate=0.0555
                               )
-cb_modelh.fit(X_train, y_train, verbose=0)
+cb_modelh.fit(X_train, y_train, verbose=50)
 
 """random_search = RandomizedSearchCV(cb_model, param_distributions=params,verbose=5)
 random_search.fit(X_train,y_train,verbose=0)
@@ -91,8 +140,8 @@ print(df)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.01, random_state=42)
 
 
-cb_modelv = CatBoostRegressor(iterations=1000,
-                              depth=6,
+cb_modelv = CatBoostRegressor(iterations=500,
+                              depth=8,
                               task_type="CPU",
                               eval_metric='R2',
                               random_seed=42,
@@ -102,7 +151,7 @@ cb_modelv = CatBoostRegressor(iterations=1000,
                               od_wait=10,
                               learning_rate=0.0555
                               )
-cb_modelv.fit(X_train, y_train, verbose=0)
+cb_modelv.fit(X_train, y_train, verbose=50)
 
 df=cleandf
 df["newkeys"]=df["newkeys"].replace("A",0)
@@ -113,6 +162,15 @@ df["newkeys"]=df["newkeys"].replace("Y",42)
 df["newkeys"]=df["newkeys"].replace("S",42)
 df["newkeys"]=df["newkeys"].replace("Q",42)
 df["newkeys"]=df["newkeys"].replace(" ",42)
+df["newkeys"]=df["newkeys"].replace(3,42)
+df["newkeys"]=df["newkeys"].replace("3",42)
+df["newkeys"]=df["newkeys"].replace("O",42)
+df["newkeys"]=df["newkeys"].replace("G",42)
+df["newkeys"]=df["newkeys"].replace("I",42)
+df["newkeys"]=df["newkeys"].replace("B",42)
+
+#df=df[~df.newkeys.str.contains("QWERTYUIOPASDFGHJKLZXCVBNM3456789")]
+
 print(df)
 
 X=df.drop('newkeys',axis=1)
@@ -125,12 +183,12 @@ for i in y:
     if i != 0:
         if i != 1:
             if i != 2:
-                print(i)
-
+                if i != 42:
+                    print(i)
 
 
 cb_modelc = CatBoostClassifier(iterations=1000,
-                             depth=6,
+                             depth=8,
                              task_type="CPU",
                              eval_metric='Accuracy',
                              random_seed=42,
@@ -140,7 +198,7 @@ cb_modelc = CatBoostClassifier(iterations=1000,
                              od_wait=10,
                              learning_rate=0.0555
                              )
-cb_modelc.fit(X_train, y_train, verbose=0)
+cb_modelc.fit(X_train, y_train, verbose=50)
 
 # save models
 
